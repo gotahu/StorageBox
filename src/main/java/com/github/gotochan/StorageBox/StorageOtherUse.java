7b89c140da6859a90cf917aba6a0a5e8c2fb487d
Hinyari_Gohan <syunsyungoto@gmail.com>
2016-07-24 10:06:46 +0900

    Revert "Revert "Ready for version 1.3""
    This reverts commit 7c6172ac304334e2fb9a58e1243d2f138e2a7207.

---
+++ src/main/java/com/github/gotochan/StorageBox/StorageOtherUse.java
@@ -0,0 +1,17 @@
+package com.github.gotochan.StorageBox;
+
+import org.bukkit.event.Listener;
+
+public class StorageOtherUse implements Listener {
+
+	private StorageBox plugin;
+	private SBUtil util;
+
+	public StorageOtherUse(StorageBox plugin) {
+		this.plugin = plugin;
+		this.util = plugin.util;
+	}
+
+
+
+}
