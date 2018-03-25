4dd27ccb1589a2bf0145b0ec841f3a51b60811d5
Hinyari_Gohan <hinyarigohan@gmail.com>
2018-03-25 23:47:22 +0900

    インベントリ関連のリスナーを追加

---
+++ src/main/kotlin/net/hinyari/plugin/storagebox/event/Inventories.kt
@@ -0,0 +1,33 @@
+package net.hinyari.plugin.storagebox.event
+
+import net.hinyari.plugin.storagebox.SBRegisterInventory
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.inventory.InventoryClickEvent
+import org.bukkit.event.inventory.InventoryCloseEvent
+import org.bukkit.event.inventory.InventoryDragEvent
+import org.bukkit.event.inventory.InventoryMoveItemEvent
+
+class Inventories : Listener {
+    
+    @EventHandler
+    fun onInventoryClickEvent(event: InventoryClickEvent) {
+        
+    }
+    
+    @EventHandler
+    fun onInventoryMoveEvent(event: InventoryMoveItemEvent) {
+        
+    }
+    
+    @EventHandler
+    fun onInventoryDragEvent(event: InventoryDragEvent) {
+        
+    }
+    
+    @EventHandler
+    fun onInventoryCloseEvent(event: InventoryCloseEvent) {
+        
+    }
+        
+}
