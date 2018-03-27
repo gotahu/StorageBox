e7efc581ba109101e4bb0b3b7fc62e743b9bb50c
Hinyari_Gohan <hinyarigohan@gmail.com>
2018-03-27 16:49:02 +0900

    インベントリの実体を実装

--- src/main/kotlin/net/hinyari/plugin/storagebox/SBRegisterInventory.kt
+++ src/main/kotlin/net/hinyari/plugin/storagebox/SBRegisterInventory.kt
@@ -3,11 +3,25 @@ package net.hinyari.plugin.storagebox
 import org.bukkit.Bukkit
 import org.bukkit.event.inventory.InventoryType
 import org.bukkit.inventory.Inventory
+import java.util.*
 
 class SBRegisterInventory {
     
     private val registerInventory = Bukkit.createInventory(null, 27, "StorageBox : 登録画面")
+    val inventories = mutableMapOf<UUID, Inventory>()
     
+    init {
+        // TODO ごにょごにょする
+    }
+    
+    fun getRegisterInventoryByUUID(uuid: UUID) : Inventory {
+        return if (inventories[uuid] != null)
+            inventories[uuid]!!
+        else {
+            inventories[uuid] = registerInventory
+            registerInventory
+        }
+    }
     
     
 }
